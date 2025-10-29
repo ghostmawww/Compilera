7de0be284a40d88e9d14b54e2e565ca11f333b50
GridinDimiterko <drplx@drplx.ru>
2025-10-29 12:35:07 +0000

    kalkulka

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,62 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float one, two, result;
+            char sign;
+            Console.WriteLine("Введи первое число, после введи (+,-,*,/) и второе число");
+            Console.WriteLine("Введи первое число");
+            one = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введи знак действия");
+            sign = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введи второе число");
+            two = Convert.ToSingle(Console.ReadLine());
+            if (sign == '+')
+            {
+                result = one + two;
+                Console.WriteLine("Сумма ваших чисел равна " + result);
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+            else if (sign == '-')
+            {
+                result = one - two;
+                Console.WriteLine("Разность ваших чисел равна " + result);
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+            else if (sign == '*')
+            {
+                result = one * two;
+                Console.WriteLine("Произведение ваших чисел равно " + result);
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+            else if (sign == '/')
+            {
+                if (two == 0)
+                {
+                    Console.WriteLine("Ошибка. Делитель не может быть равным нулю.");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    result = one / two;
+                    Console.WriteLine("Частное ваших чисел равна " + result);
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+
+
+            }
         }
     }
 }
